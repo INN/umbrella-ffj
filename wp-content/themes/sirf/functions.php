2831b390ab7b7482552a994220f3f7ec316c32e9
Josh Darby <josh@inn.org>
2020-02-20 15:23:18 -0500

    Add Grunt and LESS

--- wp-content/themes/sirf/functions.php
+++ wp-content/themes/sirf/functions.php
@@ -11,3 +11,16 @@ if ( ! defined( 'INN_MEMBER' ) ) {
 if ( ! defined( 'INN_HOSTED' ) ) {
     define( 'INN_HOSTED', true );
 }
+
+/**
+* Enqueue specific styles and scripts for sirf child theme
+*/
+function sirf_enqueue_styles(){
+	wp_enqueue_style(
+		'largo-child-styles',
+		get_stylesheet_directory_uri() . '/css/child-style.css',
+		array( 'largo-stylesheet' ),
+		filemtime( get_stylesheet_directory() . '/css/child-style.css' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'sirf_enqueue_styles' );
